e4b00e6777a850e394a4cead4ec09ea43335b84b
amanosalva <amanosalva@bytesw.com>
2018-08-16 10:49:29 -0500

    Commit 3

---
+++ webhook.py
@@ -0,0 +1,60 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+import json
+import os
+import requests
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+
+# In[ ]:
+
+
+# La aplicación Flask debe iniciarse en el diseño global
+
+app=Flask(__name__)
+
+@app.route('/webhook',methods=['POST'])
+
+def webhook():
+    req=request.get_json(silent=True force=True)
+    print(json.dumps(req, indent=4))
+    
+    res=makeResponse(req)
+    res=json.dumps(res, indent=4)
+    r=make_response(res)
+    r.headers['Content-Type']='application/json'
+    return r
+
+def makeResponse(req):
+    result=req.get("result")
+    parameters=result.get("parameters")
+    city=parameters.get("geo-city")
+    date=parameters.get("date")
+    r=requests.get('http://api.openweathermap.org/data/2.5/forecast?q=London,us&appid=b1b15e88fa797225412429c1c50c122a1')
+    json_object=r.json()
+    weather=json_object['list']
+    for i in range(0,30):
+        if date in weather[i]['dt_txt']
+        condition=weather[i]['weather'][0]['description']
+        break
+         
+    
+    speech="El pronostico para"+city+ "y para el dia"+date+" es "+condition
+    return{
+        "speech": speech,
+        "displayText": speech,
+        "source": "apiai-weather-webhook"
+    }
+
+if __name__=='__main__':
+    port=int(os.getenv('PORT',5000))
+    print("Inicio de la aplicacion en el puerto %d" % port)
+    app.run(debug=False, port=port, host='0.0.0.0')
+
