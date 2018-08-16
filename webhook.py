af6a8ee60dcc9c5ee8c67205c9fc0e09a0ac6e58
amanosalva <amanosalva@bytesw.com>
2018-08-16 11:47:48 -0500

    commit 15

--- webhook.py
+++ webhook.py
@@ -14,7 +14,7 @@ def webhook():
     req = request.get_json(silent=True, force=True)
     print(json.dumps(req, indent=4))
     
-    res = makeResponse(req)
+    res = processRequest(req)
     
     res = json.dumps(res, indent=4)
     # print(res)
@@ -31,7 +31,7 @@ def makeResponse(req):
     date = parameters.get("date")
     if city is None:
         return None
-    r=requests.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=50777fceb7cebfd4cb6f863f29199ee0')
+    r=requests.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=06f070197b1f60e55231f8c46658d077')
     json_object = r.json()
     weather=json_object['list']
     for i in range(0,30):
